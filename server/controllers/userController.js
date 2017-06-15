2eb1c893b40918bc31831fa5df104ec314ff2379
arahito <uarahito@yahoo.com>
2017-06-15 13:31:52 +0700

    make signup and signin user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,40 @@
 const User = require('../models/user')
 const methods = {}
+const bCrypt = require('bcrypt')
+const jwt =require('jsonwebtoken')
+const saltRounds = 10
 
+methods.signUp = (req, res) => {
+  let pwd = req.body.password
+  let newUser = new User({
+    name: req.body.name,
+    username: req.body.username,
+    password: bCrypt.hashSync(pwd, saltRounds),
+    email: req.body.email
+  })
 
+  newUser.save((error, response) => {
+    if (error) res.json({msg: `Something error signup: ${error}`, success: false})
+    else {
+      console.log('Signup success');
+      console.log(response);
+      res.send(response)
+    }
+  })
+}
+
+methods.signIn = (req, res) => {
+  let pwd = req.body.password
+  User.findOne({ username: req.body.username}, (error, response) => {
+    if (error) res.json({msg: `Something error signup: ${error}`, success: false})
+    else {
+      if (bCrypt.compareSync(response.password, pwd)) {
+        let token = jwt.sign({
+          id:
+        })
+      }
+    }
+  })
+}
 
 module.exports = methods
